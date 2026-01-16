550b9c3c2b8e83058f888489c16bf8faf24a6fbf
Ryboster <kakashise@o2.pl>
2026-01-16 19:33:27 +0000

    added pause

---
+++ Assets/Resources/UI/Scripts/PauseButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class PauseButton : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public Sprite ResumeIcon;
+
+    private Sprite _fallbackIcon;
+    private Image _imageComponent;
+
+    public void ToggleMenu() 
+    { 
+        PauseMenu.SetActive(!PauseMenu.activeSelf); 
+
+        if (PauseMenu.activeSelf) _imageComponent.sprite = ResumeIcon;
+    }
+    void Start() 
+    {
+        if (!PauseMenu) Debug.LogWarning("PauseButton.cs: PauseMenu:GameObject is null."); 
+        _imageComponent = gameObject.GetComponent<Image>();
+        _fallbackIcon = _imageComponent.sprite;
+    }
+    
+    void FixedUpdate()
+    {
+        if (!PauseMenu.activeSelf) _imageComponent.sprite = _fallbackIcon;
+    }
+}
