e35e84e733df96c1d5970364768c21ce32883c01
Ryboster <kakashise@o2.pl>
2026-02-26 08:04:23 +0000

    added shop UI

---
+++ Assets/Resources/UI/Windows/Shop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    public List<GameObject> toolsCatalogue;
+    public List<GameObject> upgradesCatalogue;
+    
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/UI/Windows/ShopItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+
+    public Sprite thumbnail;
+    public int price;
+    public string description;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
